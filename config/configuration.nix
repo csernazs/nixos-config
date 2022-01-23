8776fdfcb37a20901fa5906ea7da071c522cd952
Cserna Zsolt <cserna.zsolt@gmail.com>
2022-01-23 22:36:46 +0100

    intel-ocl: fix src url
    Replace https to http in the url.
    See https://github.com/NixOS/nixpkgs/issues/155664

--- config/configuration.nix
+++ config/configuration.nix
@@ -206,4 +206,18 @@
   # should.
   system.stateVersion = "19.09"; # Did you read the comment?
 
+
+  nixpkgs.overlays = [
+    (self: super: {
+      intel-ocl = super.intel-ocl.overrideAttrs
+        (old: {
+          src = pkgs.fetchzip {
+            url = "http://registrationcenter-download.intel.com/akdlm/irc_nas/11396/SRB5.0_linux64.zip";
+            sha256 = "0qbp63l74s0i80ysh9ya8x7r79xkddbbz4378nms9i7a0kprg9p2";
+            stripRoot = false;
+          };
+        });
+    }
+    )
+  ];
 }
